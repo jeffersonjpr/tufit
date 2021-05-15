f85e2863a0241a4bd19ba541b77d9b0161492637
LordNicholasxd <nikkiextremer@gmail.com>
2021-05-15 15:54:13 -0300

    Fiz A pagina De Cadastro(AInda FALTA ARRUMAR A APARENCIA E GRAVAR OS DADOS NO FIREBASE)

--- lib/pages/cadastro_page.dart
+++ lib/pages/cadastro_page.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:tufit/controllers/autenticacao_controller.dart';
 
 class CadastroPage extends StatelessWidget {
+  String dropdownValue = 'Masculino';
   final controller = Get.put(AutenticacaoController());
   //implementar o acesso ao db
   //implementar o firebase.storage para guardar a imagem
@@ -12,6 +13,139 @@ class CadastroPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Cadastre-se'),
+      ),
+      body: Obx(
+        () => controller.isLoading.value
+            ? Center(child: CircularProgressIndicator())
+            : Form(
+                key: controller.formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(24),
+                      child: TextFormField(
+                        controller: controller.email,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Email',
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return 'Email invalido!';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                          vertical: 12.0, horizontal: 24.0),
+                      child: TextFormField(
+                        controller: controller.senha,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Senha',
+                        ),
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return 'Informa sua senha!';
+                          } else if (value.length < 6) {
+                            return 'Sua senha deve ter no mínimo 6 caracteres';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(24),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Nome',
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(24),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Idade',
+                        ),
+                      ),
+                    ),
+                    Text('Genero'),
+                    DropdownButton<String>(
+                      value: dropdownValue,
+                      icon: const Icon(Icons.arrow_downward),
+                      iconSize: 24,
+                      elevation: 16,
+                      style: const TextStyle(color: Colors.deepPurple),
+                      underline: Container(
+                        height: 2,
+                        color: Colors.blue,
+                      ),
+                      onChanged: (String newValue) {
+                        dropdownValue = newValue;
+                      },
+                      items: <String>[
+                        'Masculino',
+                        'Feminino',
+                        'Helicopetero',
+                        'Monster Energy Drink'
+                      ].map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(24),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Altura(m)',
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(24),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Peso(kg)',
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(24.0),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          if (controller.formKey.currentState.validate()) {
+                            controller.login();
+                          }
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.check),
+                            Padding(
+                              padding: EdgeInsets.all(16.0),
+                              child: Obx(() => Text(
+                                  controller.botaoPrincipal.value,
+                                  style: TextStyle(fontSize: 20))),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
       ),
     );
   }
